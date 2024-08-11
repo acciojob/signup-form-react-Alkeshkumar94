9dac5399a9a4b8fdefaf2e2ed1b4651e765f0f26
Alkeshkumar94 <alkeshkumar949@gmail.com>
2024-08-11 17:34:21 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,130 @@
-import React, {Component, useState} from "react";
+import React, { useState } from "react";
 import '../styles/App.css';
 
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
-}
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    gender: "male",
+    phoneNumber: "",
+    password: "",
+  });
+  
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
 
+  const validateForm = () => {
+    const { name, email, gender, phoneNumber, password } = formData;
+    setError("");
+
+    if (!name || !email || !phoneNumber || !password) {
+      return 'All fields are mandatory.';
+    }
+    if (!/^[a-zA-Z0-9 ]+$/.test(name)) {
+      return 'Name is not alphanumeric.';
+    }
+    if (!email.includes('@')) {
+      return 'Email must contain @.';
+    }
+    if (!['male', 'female', 'other'].includes(gender)) {
+      return 'Please identify as male, female or others.';
+    }
+    if (!/^\d+$/.test(phoneNumber)) {
+      return 'Phone Number must contain only numbers.';
+    }
+    if (password.length < 6) {
+      return 'Password must contain at least 6 letters.';
+    }
+    return "";
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationError = validateForm();
+    
+    if (validationError) {
+      setError(validationError);
+      setMessage('');
+    } else {
+      const username = formData.email.split('@')[0];
+      setMessage(`Hello ${username}`);
+      setError('');
+    }
+  };
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  return (
+    <div id="main">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input 
+            name="name" 
+            value={formData.name} 
+            data-testid='name' 
+            type="text" 
+            onChange={handleInput}
+          />
+        </div>
+        <br />
+        <div>
+          <label>Email address:</label>
+          <input 
+            name="email" 
+            value={formData.email} 
+            onChange={handleInput} 
+            data-testid='email' 
+          />
+        </div>
+        <br />
+        <div>
+          <label>Gender:</label>
+          <select 
+            value={formData.gender} 
+            onChange={handleInput} 
+            name="gender" 
+            data-testid='gender'
+          >
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <br />
+        <div>
+          <label>Phone Number:</label>
+          <input 
+            value={formData.phoneNumber} 
+            onChange={handleInput} 
+            name="phoneNumber" 
+            data-testid='phoneNumber' 
+            type="text"
+          />
+        </div>
+        <br />
+        <div>
+          <label>Password:</label>
+          <input 
+            value={formData.password} 
+            onChange={handleInput} 
+            name="password" 
+            data-testid='password' 
+            type="password"
+          />
+        </div>
+        <br />
+        <div>
+          <button type="submit" data-testid='submit'>Submit</button>
+        </div>
+      </form>
+      {error && <div className="error">{error}</div>}
+      {message && <div className="welcome">{message}</div>}
+    </div>
+  );
+}
 
 export default App;
